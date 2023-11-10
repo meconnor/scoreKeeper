acd864cfb42b3008042979c3231bb6f93c246211
Connor <c.meriweather55@gmail.com>
2023-11-10 17:08:26 -0600

    added reset button functionality.

--- app.js
+++ app.js
@@ -2,25 +2,43 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
-
+const resetButton = document.querySelector('#reset');
 
 
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false;
+
 
 
 p1Button.addEventListener('click', function() {
-    if(p1Score !== winningScore) {
+    if(!isGameOver) {
         p1Score++;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
         p1Display.textContent = p1Score;
     }
-    
 })
+
+
 p2Button.addEventListener('click', function() {
-    if(p2Score !== winningScore) {
+    if(!isGameOver) {
         p2Score++;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
         p2Display.textContent = p2Score;
     }
-    
+})
+
+
+resetButton.addEventListener('click', function () {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+
 })
