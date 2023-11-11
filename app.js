f0ef3ed0d1c500ebe3114ed8cd9dd89a58de1ff5
Connor <c.meriweather55@gmail.com>
2023-11-11 08:16:03 -0600

    changed functionality of reset button to match score selector, added winningScore event listener to stop at selected score

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
 
 
 let p1Score = 0;
@@ -33,12 +34,21 @@ p2Button.addEventListener('click', function() {
     }
 })
 
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
 
-resetButton.addEventListener('click', function () {
+resetButton.addEventListener('click', reset)
+
+
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
+}
 
-})
+
+
